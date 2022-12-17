49ea8343210c2db29522a4185afd14dbb69be599
tkawabat <tosiki.kawabata@gmail.com>
2022-12-17 16:34:22 +0900

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
 import 'component/l5/my_home_page.dart';
 
 Future main() async {
   const flavor = String.fromEnvironment('FLAVOR');
   await dotenv.load(fileName: 'assets/env.$flavor');
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const ProviderScope(child: MyApp()));
 }
