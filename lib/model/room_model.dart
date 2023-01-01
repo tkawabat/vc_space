cd10d9bd47f6fa0fb4e4e50d9c1d301261462928
tkawabat <tosiki.kawabata@gmail.com>
2023-01-01 21:23:31 +0900

    fix

--- lib/model/room_model.dart
+++ lib/model/room_model.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:vc_space/entity/room_entity.dart';
 import 'package:vc_space/model/model_base.dart';
 
