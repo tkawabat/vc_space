c76ecddd20b08f03d64c95ca062b4efebdf4656d
tkawabat <tosiki.kawabata@gmail.com>
2023-01-01 21:13:09 +0900

    javascript内のuser作成コードを修正

--- web/js/twitter.js
+++ web/js/twitter.js
@@ -14,12 +14,14 @@ async function twitterLogin() {
             id: result.user.uid,
             name: result._tokenResponse.fullName,
             photo: result._tokenResponse.photoUrl,
+            tags: [],
             twitterId: result._tokenResponse.screenName,
             // twitter_token: result._tokenResponse.oauthAccessToken,
             // twitter_secret: result._tokenResponse.oauthTokenSecret,
         }
         return import(FIREBASE_URL_BASE+'/firebase-firestore.js')
     }).then((module) => {
+        userData.updatedAt = module.serverTimestamp()
         userRef = module.doc(module.getFirestore(), 'user', userData.id)
         module.setDoc(userRef, userData, { merge: true}).catch((error) => {
             alert('ユーザーデータ登録エラー.' + error)
