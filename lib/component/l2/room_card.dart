156fca239e4313aa0b5f04488c9da185810afb1f
tkawabat <tosiki.kawabata@gmail.com>
2023-01-05 20:35:51 +0900

    部屋カードを修正

--- lib/component/l2/room_card.dart
+++ lib/component/l2/room_card.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:vc_space/component/l1/user_icon.dart';
+import 'package:vc_space/service/const_design.dart';
 
 import '../../entity/room_entity.dart';
+import '../l1/tag.dart';
 import '../l5/room_detail_page.dart';
 
 class RoomCard extends StatelessWidget {
@@ -8,19 +12,15 @@ class RoomCard extends StatelessWidget {
 
   const RoomCard({super.key, required this.room});
 
+  void onTap(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => RoomDetailPage(room: room)));
+  }
+
   @override
   Widget build(BuildContext context) {
-    const colorPrimary = Colors.deepOrangeAccent;
-    const colorPositive = Colors.greenAccent;
-    // const colorNegative = Colors.deepOrangeAccent;
-
     return InkWell(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => RoomDetailPage(room: room)));
-        },
+        onTap: () => onTap(context),
         child: Container(
             margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
             child: Card(
@@ -32,84 +32,66 @@ class RoomCard extends StatelessWidget {
               child: Column(
                 children: [
                   ListTile(
-                    leading: ClipOval(
-                      child: Container(
-                        color: colorPrimary,
-                        width: 48,
-                        height: 48,
-                        child: Center(
-                          child: Text(
-                            room.title.substring(0, 1),
-                            style: const TextStyle(
-                                color: Colors.white, fontSize: 24),
-                          ),
-                        ),
-                      ),
+                    leading: UserIcon(
+                      photo: room.ownerImage,
+                      tooltip: 'ユーザー情報を見る',
+                      onTap: () {},
                     ),
                     title: Text(room.title),
-                    subtitle: const Text('2 min ago'),
+                    trailing: buildTrailing(room),
                   ),
                   Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(width: 72),
-                        Container(
-                          width: 16,
-                          height: 16,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: colorPrimary, width: 4),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        const Flexible(child: Text('aaaaaaa')),
-                      ],
+                    padding: const EdgeInsets.fromLTRB(8, 4, 8, 8),
+                    alignment: Alignment.topLeft,
+                    child: Wrap(
+                      spacing: 2,
+                      children: buildTag(room),
                     ),
-                  ),
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Row(
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(color: colorPrimary, width: 2),
-                            ),
-                          ),
-                          child: const Text(
-                            'reason aaaaaaaaaaa',
-                            style: TextStyle(color: Colors.blueAccent),
-                          ),
-                        ),
-                        const SizedBox(width: 24),
-                        Expanded(
-                          child: TextButton(
-                            style: TextButton.styleFrom(
-                                // primary: colorNegative,
-                                ),
-                            onPressed: () {},
-                            child: const Text('negative aaaaaaaaaa'),
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        Expanded(
-                          child: TextButton(
-                            style: TextButton.styleFrom(
-                              foregroundColor: colorPositive,
-                              backgroundColor: colorPositive.withOpacity(0.2),
-                            ),
-                            onPressed: () {},
-                            child: const Text('positive aaaaaaaa'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  )
                 ],
               ),
             )));
+  }
+
+  Widget buildTrailing(room) {
+    DateFormat formatter = DateFormat('M/d(E) HH:mm', "ja_JP");
+    String start = formatter.format(room.startTime);
+    String numberText = '${room.users.length}/${room.maxNumber}';
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text('$start~'),
+        SizedBox(
+            width: 40,
+            height: 14,
+            child: Row(
+              children: [
+                const Icon(Icons.person, size: 18),
+                Text(numberText),
+              ],
+            )),
+      ],
+    );
+  }
+
+  List<Widget> buildTag(RoomEntity room) {
+    List<Widget> widgets = room.tags
+        .map((text) => Tag(
+              text: text,
+              tagColor: ConstDesign.validTagColor,
+              onTap: () {},
+            ))
+        .toList();
+
+    widgets.insert(
+        0,
+        Tag(
+          text: room.place.displayName,
+          tagColor: Colors.cyan.shade100,
+          onTap: () {},
+        ));
+
+    return widgets;
   }
 }
