0fdd5d3a0b4a28e721c66e4bb8b2049fd3446180
tkawabat <tosiki.kawabata@gmail.com>
2023-01-06 16:14:17 +0900

    起動時にroom listを取得するようにした

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'firebase_options.dart';
+import 'provider/room_list_provider.dart';
 import 'route.dart';
 
 import 'component/page/main_page.dart';
@@ -30,6 +31,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     listenFirebaseAuth(ref);
+    ref.read(roomListProvider.notifier).get();
 
     return MaterialApp(
       title: dotenv.get('TITLE'),
