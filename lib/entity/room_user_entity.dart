ac0e2089fc5fc129b98d2106c3c89921d999b206
tkawabat <tosiki.kawabata@gmail.com>
2023-02-05 21:15:31 +0900

    RoomUserEntityを修正

--- lib/entity/room_user_entity.dart
+++ lib/entity/room_user_entity.dart
@@ -1,32 +1,43 @@
 // ignore_for_file: invalid_annotation_target
 
-// json_serialize内で使っているので消しちゃだめ
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import 'converter/datetime_converter.dart';
+import 'converter/timestampz_converter.dart';
 import 'converter/room_user_type_converter.dart';
 
 part 'room_user_entity.freezed.dart';
 part 'room_user_entity.g.dart';
 
 enum RoomUserType {
-  admin('主催者'),
-  member('参加者'),
-  offer('オファー中'),
+  admin(10, '主催者'),
+  member(20, '参加者'),
+  offer(80, 'オファー中'),
   ;
 
-  const RoomUserType(this.displayName);
+  const RoomUserType(this.value, this.displayName);
+  final int value;
   final String displayName;
+
+  factory RoomUserType.fromValue(int value) {
+    switch (value) {
+      case 10:
+        return RoomUserType.admin;
+      case 20:
+        return RoomUserType.member;
+      case 30:
+        return RoomUserType.offer;
+    }
+    throw Exception('RoomUserType error. value=$value');
+  }
 }
 
 @freezed
 class RoomUserEntity with _$RoomUserEntity {
   const factory RoomUserEntity({
-    required String id,
-    required String photo,
+    required int roomId,
+    required String uid,
     @RoomUserTypeConverter() required RoomUserType roomUserType,
-    @DateTimeConverter() required DateTime updatedAt,
+    @TimestampzConverter() required DateTime updatedAt,
   }) = _RoomUserEntity;
 
   factory RoomUserEntity.fromJson(Map<String, dynamic> json) =>
